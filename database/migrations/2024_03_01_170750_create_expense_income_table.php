11b48683b9c33c5b39142456c0982cbd2c4c91ef
malierdgn <malierdgnn@gmail.com>
2024-03-02 14:47:35 +0300

    Initial Commit

---
+++ database/migrations/2024_03_01_170750_create_expense_income_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expense_income', function (Blueprint $table) {
+            $table->id();
+            $table->integer("amount");
+            $table->tinyInteger("type");
+            $table->dateTime("time");
+            $table->string("description");
+            $table->unsignedBigInteger("tag_id");
+            $table->unsignedBigInteger("method_id");
+            $table->timestamps();
+
+            $table->foreign("tag_id")->references("id")->on("tags");
+            $table->foreign("method_id")->references("id")->on("methods");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expense_income');
+    }
+};
