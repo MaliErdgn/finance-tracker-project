e50c1b8e439dfcea67d806c37a3099651ccf6837
MaliErdgn <malierdgnn@gmail.com>
2024-03-07 12:39:58 +0300

    Editing data function
    Made progress on the function. Moved it from dataSubmit.jsx to Dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
-import { Table } from 'react-bootstrap';
+import { Table, Button, ButtonGroup } from 'react-bootstrap';
 import Chart from 'chart.js/auto';
 
 const Balance = () => {
@@ -8,6 +8,14 @@ const Balance = () => {
     const [totalIncome, setTotalIncome] = useState();
     const [totalExpense, setTotalExpense] = useState();
     const [totalBalance, setTotalBalance] = useState();
+    const [selectedBalanceId, setSelectedBalanceId] = useState(null);
+    const [editedData, setEditedData] = useState({ ...data });
+    const [categories, setCategories] = useState([]);
+    const [allTags, setAllTags] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("");
+    const [filteredTags, setFilteredTags] = useState([]);
+    const [types, setTypes] = useState([]);
+    const [methods, setMethods] = useState([]);
 
 
     useEffect(() => {
@@ -15,12 +23,23 @@ const Balance = () => {
 
         const fetchData = async () => {
             try {
-                const response = await axios.get("/api/balance");
+                const [categoriesResponse, tagsResponse, typesResponse, methodsResponse, dataResponse] = await Promise.all([
+                    axios.get("/api/categories"),
+                    axios.get("/api/tags"),
+                    axios.get("/api/types"),
+                    axios.get("/api/methods"),
+                    axios.get("/api/balance")
+                ]);
+
+                setCategories(categoriesResponse.data);
+                setAllTags(tagsResponse.data);
+                setTypes(typesResponse.data);
+                setMethods(methodsResponse.data)
+                setData(dataResponse.data)
                 if (mounted) {
-                    const timeSortedData = response.data.sort((a, b) => new Date(a.time) - new Date(b.time))
+                    const timeSortedData = dataResponse.data.sort((a, b) => new Date(a.time) - new Date(b.time))
                     setData(timeSortedData);
                     calculateTotals(timeSortedData);
-                    createChart(timeSortedData);
                 }
             } catch (error) {
                 console.error("Data gelmedi", error);
@@ -32,10 +51,6 @@ const Balance = () => {
         return () => {
             mounted = false; // Prevent state update after unmount
 
-            // Destroy the pie chart instance when the component unmounts
-            if (pieChartInstance.current) {
-                pieChartInstance.current.destroy();
-            }
         };
     }, []);
 
@@ -57,6 +72,15 @@ const Balance = () => {
         setTotalBalance(income - expense);
     };
 
+    const handleEdit = (id, category, tag, method, type) => {
+        console.log("Editing Props props:",  id, category, tag, method, type);
+        setSelectedBalanceId(id);
+    }
+
+    const handleDelete = (id) => {
+        console.log("Deleting balance with ID:", id)
+        setSelectedBalanceId(id);
+    }
 
     const formatDate = (dateString) => {
         const options = { day: "2-digit", month: "2-digit", year: "numeric" }
@@ -74,6 +98,7 @@ const Balance = () => {
                         <th scope="col">Category Name</th>
                         <th scope="col">Method</th>
                         <th scope="col">Description</th>
+                        <th scope="col">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -86,6 +111,15 @@ const Balance = () => {
                             <td>{item.tag.category.category_name}</td>
                             <td>{item.method.method_name}</td>
                             <td>{item.description}</td>
+                            <td>
+                                    <Button type="button" variant="info" className="mr-3" onClick={() =>
+                                        handleEdit(item.id,
+                                                    item.tag.category.category_name,
+                                                    item.tag.tag_name,
+                                                    item.method.method_name,
+                                                    item.type.type_name)}>Edit</Button>
+                                    <Button type="button" variant="danger" onClick={() => handleDelete(item.id)}>Delete</Button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
